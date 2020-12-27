4777bad5de99ab592725dc9ede62d18dbee465c4
John Lee <johnmlee1204@gmail.com>
2020-12-27 16:57:06 -0600

    Started on Pet Master

--- PetMaster/php/Models/PetMasterModel.php
+++ PetMaster/php/Models/PetMasterModel.php
@@ -1,7 +1,119 @@
 <?php
 
+namespace PetMaster\Models;
+use AgileModel;
 
-class PetMasterModel
-{
+class PetMasterModel extends AgileModel {
+	static function readPet($petId) {
+		return self::$database->fetch_assoc("
+			SELECT
+				petId,
+				name,
+				type,
+				price,
+				birthDate,
+				receiveDate,
+				sellDate,
+				vendor,
+				cost,
+				habitatId,
+				food,
+				feedingQuantity,
+				feedingFrequency,
+				customer
+			FROM Pet
+			WHERE
+				petId = ?
+		", [$petId]);
+	}
 
+	static function createPet($inputs) {
+		self::$database->query("
+			INSERT INTO Pet(
+				name,
+				type,
+				price,
+				birthDate,
+				receiveDate,
+				sellDate,
+				vendor,
+				cost,
+				habitatId,
+				food,
+				feedingQuantity,
+				feedingFrequency,
+				customer
+			)
+			VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)
+		", [
+			$inputs['name'],
+			$inputs['type'],
+			$inputs['price'],
+			$inputs['birthDate'],
+			$inputs['receiveDate'],
+			$inputs['sellDate'],
+			$inputs['vendor'],
+			$inputs['cost'],
+			$inputs['habitatId'],
+			$inputs['food'],
+			$inputs['feedingQuantity'],
+			$inputs['feedingFrequency'],
+			$inputs['customer']
+		]);
+
+		return self::readLatestPetId();
+	}
+
+	static function updatePet($inputs) {
+		self::$database->query("
+			UPDATE Pet
+			SET
+				name = ?,
+				type = ?,
+				price = ?,
+				birthDate = ?,
+				receiveDate = ?,
+				sellDate = ?,
+				vendor = ?,
+				cost = ?,
+				habitatId = ?,
+				food = ?,
+				feedingQuantity = ?,
+				feedingFrequency = ?,
+				customer = ?
+			WHERE
+				petId = ?
+		", [
+			$inputs['name'],
+			$inputs['type'],
+			$inputs['price'],
+			$inputs['birthDate'],
+			$inputs['receiveDate'],
+			$inputs['sellDate'],
+			$inputs['vendor'],
+			$inputs['cost'],
+			$inputs['habitatId'],
+			$inputs['food'],
+			$inputs['feedingQuantity'],
+			$inputs['feedingFrequency'],
+			$inputs['customer'],
+			$inputs['petId']
+		]);
+	}
+
+	static function deletePet($petId) {
+		self::$database->query("
+			DELETE FROM Pet
+			WHERE
+				petId = ?
+		", [$petId]);
+	}
+
+	static function readLatestPetId() {
+		$petId = self::$database->fetch_assoc("
+			SELECT MAX(petId) petId FROM Pet
+		");
+
+		return $petId['petId'];
+	}
 }
--- PetMaster/php/PetMaster.php
+++ PetMaster/php/PetMaster.php
@@ -1,7 +1,66 @@
 <?php
 
+use DropDownSelectionEditor\Models\DropDownSelectionEditorModel;
+use PetMaster\Models\PetMasterModel;
+class PetMaster extends AgileBaseController {
+	function readPet() {
+		$input = Validation::validateJsonInput([
+			'petId' => 'numeric'
+		]);
 
-class PetMaster
-{
+		$this->outputSuccessData(PetMasterModel::readPet($input['petId']));
+	}
 
+	function createPet() {
+		$inputs = Validation::validateJsonInput([
+			'name' => 'notBlank',
+			'type' => 'notBlank',
+			'price' => 'numeric',
+			'birthDate',
+			'receiveDate' => 'notBlank',
+			'sellDate',
+			'vendor',
+			'cost',
+			'habitatId',
+			'food',
+			'feedingQuantity',
+			'feedingFrequency',
+			'customer'
+		]);
+
+		$this->outputSuccessData(PetMasterModel::createPet($inputs));
+	}
+
+	function updatePet() {
+		$inputs = Validation::validateJsonInput([
+			'petId' => 'numeric',
+			'name' => 'notBlank',
+			'type' => 'notBlank',
+			'price' => 'numeric',
+			'birthDate',
+			'receiveDate' => 'notBlank',
+			'sellDate',
+			'vendor',
+			'cost',
+			'habitatId',
+			'food',
+			'feedingQuantity',
+			'feedingFrequency',
+			'customer'
+		]);
+
+		PetMasterModel::updatePet($inputs);
+
+		$this->outputSuccess();
+	}
+
+	function deletePet() {
+		$input = Validation::validateJsonInput([
+			'petId' => 'numeric'
+		]);
+
+		PetMasterModel::deletePet($input['petId']);
+
+		$this->outputSuccess();
+	}
 }
